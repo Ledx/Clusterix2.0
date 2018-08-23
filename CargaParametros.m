a2cdd9c9c5c0a6b1efc77b000e44f03333796b9f
Ledx <colmillo4112@gmail.com>
2018-08-22 23:36:24 -0500

    Version con generacion de archivos de salida vacios

--- CargaParametros.m
+++ CargaParametros.m
@@ -1,18 +1,21 @@
-function [REPETICIONES,ITERACIONES,L,N,A_POTENCIAL,ALFA_POTENCIAL,R,DELTA_AVANCE,DELTA_ROTACION,W,SIGMA_ROTACION,R_COLISION]= CargaParametros()
+function [REPETICIONES,ITERACIONES,L,N,A_POTENCIAL,ALFA_POTENCIAL,R,DELTA_AVANCE,DELTA_ROTACION,W,SIGMA_ROTACION,R_COLISION,ACTIVAR_GRAFICOS,TAM_IDENTIFICADOR]= CargaParametros()
 %Carga de los parametros iniciales de entrada
     
-  REPETICIONES= 1;
-  ITERACIONES = 1000;
-  L = 300;
-  N = 9;
-  A_POTENCIAL = [0.9990,1.0010];
-  ALFA_POTENCIAL = 2;
-  R = 3;
-  DELTA_AVANCE = R/2;
-  DELTA_ROTACION = pi/6;
-  W=[1 0 0 0 0 0 0 0 0];
-  SIGMA_ROTACION = 0;
-  R_COLISION = R/2;
+  REPETICIONES= 1;              %Numero de experimentos a realizar
+  ITERACIONES = 1000;           %Numero de iteraciones por experimento
+  L = 300;                      %Longitud del area de lanzamiento de medidas L x L
+  N = 9;                        %Numero de robots
+  A_POTENCIAL = [0.9990,1.0010];%Constante de potencial
+  ALFA_POTENCIAL = 2;           %Exponente alfa de potencial
+  R = 3;                        %Radio de activacion
+  DELTA_AVANCE = R/2;           %Delta de avance
+  DELTA_ROTACION = pi/6;        %Delta de rotacion
+  W=[1 0 0 0 0 0 0 0 0];        %Pesos de las distintas frecuencias
+  SIGMA_ROTACION = 0;           %Sigma para incertezas de rotacion
+  R_COLISION = R/2;             %Radio de colision
+  ACTIVAR_GRAFICOS = 1;         %Bandera para indicar si se desean desplegar los graficos en tiempo real
+  TAM_IDENTIFICADOR = 6;        %Establece la longitud del identificador de los archivos de salida
+  
 %   save('Parametros.mat','REPETICIONES','ITERACIONES','L','N','A_POTENCIAL','ALFA_POTENCIAL','R','DELTA_AVANCE','DELTA_ROTACION','W','SIGMA_ROTACION','R_COLISION');
 %   load('Parametros','-mat','REPETICIONES','ITERACIONES','L','N','A_POTENCIAL','ALFA_POTENCIAL','R','DELTA_AVANCE','DELTA_ROTACION','W','SIGMA_ROTACION','R_COLISION');
   
--- Inicio.m
+++ Inicio.m
@@ -10,7 +10,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Desarrollador: Ing. Led Eduardo Ruvalcaba Tamés
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Actualizado 09/07/18 0036 hrs UTC-6
+%Actualizado 22/08/18 1135 hrs UTC-6
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear all;
@@ -20,17 +20,25 @@ close all;
 format long
 
 %Carga de parametros iniciales
-[REPETICIONES,ITERACIONES,L,N,A_POTENCIAL,ALFA_POTENCIAL,R,DELTA_AVANCE,DELTA_ROTACION,W,SIGMA_ROTACION,R_COLISION]=CargaParametros();
+[REPETICIONES,ITERACIONES,L,N,A_POTENCIAL,ALFA_POTENCIAL,R,DELTA_AVANCE,DELTA_ROTACION,W,SIGMA_ROTACION,R_COLISION,ACTIVAR_GRAFICOS,TAM_IDENTIFICADOR]=CargaParametros();
 colores = ['c*';'m*';'y*';'m+';'g*';'b*';'y+';'r*';'c+'];
+%Creacion de manejadores de archivos
+identificador = strcat(CreaIdentificador(TAM_IDENTIFICADOR),'-',date);
+api=CreaArchivo(identificador,1);
+his=CreaArchivo(identificador,2);
+clu=CreaArchivo(identificador,3);
+%fprintf(api,'%d \n',[REPETICIONES,ITERACIONES,L,N,A_POTENCIAL,ALFA_POTENCIAL,R,DELTA_AVANCE,DELTA_ROTACION,W,SIGMA_ROTACION,R_COLISION,ACTIVAR_GRAFICOS,TAM_IDENTIFICADOR]); 
 %Simulacion
 ci=1;
 cr=1;
-tic
+
 while cr<REPETICIONES + 1
-    robots= Inicializacion(N,L,R,A_POTENCIAL);
-    figure(cr)
-    axis([-L 2*L -L 2*L]);
-    grid
+    robots= Inicializacion(N,L,R,A_POTENCIAL); 
+    if ACTIVAR_GRAFICOS
+        figure(cr)
+        axis([-L 2*L -L 2*L]);
+        grid
+    end
     while ci<ITERACIONES + 1
         %Revision de colisiones entre robots
         for h=1:N
@@ -40,36 +48,45 @@ while cr<REPETICIONES + 1
                     robots(h).clase = robots(h).clase + 1;
                     robots(l).clustered = 1;
                     robots(l).clase = robots(l).clase + 1;
-                    [x,y]=GeneradorCircunferencia([robots(h).posicionRect.x,robots(h).posicionRect.y],[robots(l).posicionRect.x,robots(l).posicionRect.y]);
-                    hold on
-                    plot(x,y,'k');
-                    drawnow;
+                    if ACTIVAR_GRAFICOS
+                        [x,y]=GeneradorCircunferencia([robots(h).posicionRect.x,robots(h).posicionRect.y],[robots(l).posicionRect.x,robots(l).posicionRect.y]);
+                        hold on
+                        plot(x,y,'k');
+                        drawnow;
+                    end
                 end
             end
         end
-        if ci > 1
-            for i=1:N
-                hold on
-                plot(robots(i).posicionRect.x,robots(i).posicionRect.y,colores(i,:));
-                drawnow;
-            end
-        else
-            for i=1:N
-                hold on
-                plot(robots(i).posicionRect.x,robots(i).posicionRect.y,'ko');
-                drawnow;
+        if ACTIVAR_GRAFICOS
+            if ci > 1
+                for i=1:N
+                    hold on
+                    plot(robots(i).posicionRect.x,robots(i).posicionRect.y,colores(i,:));
+                    drawnow;
+                end
+            else
+                for i=1:N
+                    hold on
+                    plot(robots(i).posicionRect.x,robots(i).posicionRect.y,'ko');
+                    drawnow;
+                end
             end
         end
         robots=RotacionAvance(robots,DELTA_ROTACION,N,W,ALFA_POTENCIAL,DELTA_AVANCE,SIGMA_ROTACION);
         ci=ci+1;
     end
-    for i=1:N
-        hold on
-        plot(robots(i).posicionRect.x,robots(i).posicionRect.y,'k*');
-        drawnow;
+    if ACTIVAR_GRAFICOS
+        for i=1:N
+            hold on
+            plot(robots(i).posicionRect.x,robots(i).posicionRect.y,'k*');
+            drawnow;
+        end
+        hold off
     end
-    hold off
     cr=cr+1;
     ci=1;
 end
-toc
+fclose(api);
+fclose(his);
+fclose(clu);
+
